7cb3e380743a75868c13c77dcd13473e7354cc3b
Matt Guttman <msguttman@gmail.com>
2020-04-23 21:30:33 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,11 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  my_variable = count_strings(array)
+  my_variable
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  my_variable1 = count_empty_strings(array)
+  my_variable1
 end
